db5c0ca5ade0b177ffee1f43e02772dc0468f486
Vinzenz Wetzel <67996371+DasEvoli@users.noreply.github.com>
2022-12-11 21:30:04 +0100

    Fixed missing semicolon

--- src/feature/savecheck/feat_savecheck.cpp
+++ src/feature/savecheck/feat_savecheck.cpp
@@ -23,7 +23,7 @@ namespace sv::feature::savecheck {
         } else if (is_version("1.0.1")) {
             SaveDataCheckHook::InstallAtOffset(0x12e121c);
         } else if (is_version("1.1.0")) {
-            SaveDataCheckHook::InstallAtOffset(0x131eadc)
+            SaveDataCheckHook::InstallAtOffset(0x131eadc);
         } else {
             EXL_ASSERT(false, "Unknown version");
         }
