6f5d1f02065101a0bf4f8fddffc8671b23761186
Martmists <martmists@gmail.com>
2022-12-16 20:56:46 +0100

    remove hacky mess
    Signed-off-by: Martmists <martmists@gmail.com>

--- src/mod/main.cpp
+++ src/mod/main.cpp
@@ -18,43 +18,6 @@
 #include "feat_romfs.h"
 #endif
 
-#ifdef LUA_ENABLED
-#ifdef LOGGING_ENABLED
-#define CUSTOM_ENABLED 1
-
-static bool (*lua_toboolean)(void *L, int idx);
-
-#define Lua_String_Arg_Hook(N, boolean) \
-HOOK_DEFINE_TRAMPOLINE(Hook_##N) { \
-    static void* Callback(lua_State *L) { \
-        const char* param = lua_tolstring(L, 2, nullptr); \
-        if (boolean) { \
-            bool b = lua_toboolean(L, 3); \
-            Logger::log("Lua Hook #%08x String Arg: %s | %d\n", N, param, b); \
-        } else {                        \
-            Logger::log("Lua Hook #%08x String Arg: %s\n", N, param); \
-        } \
-        return Orig(L); \
-    } \
-};
-
-Lua_String_Arg_Hook(0x165d8c0, false)
-Lua_String_Arg_Hook(0x179bdd0, true)
-Lua_String_Arg_Hook(0x23e0780, true)
-Lua_String_Arg_Hook(0x2411d30, true)
-Lua_String_Arg_Hook(0x2421560, false)
-Lua_String_Arg_Hook(0x24264b0, true)
-
-HOOK_DEFINE_TRAMPOLINE(StringsFuncHook) {
-    static void* Callback(void* p1, void* p2, char* p3, char* p4) {
-        Logger::log("Lua Hook Strings: %s, %s\n", p3, p4);
-        return Orig(p1, p2, p3, p4);
-    }
-};
-
-#endif
-#endif
-
 extern "C" void exl_main(void* x0, void* x1) {
     /* Setup hooking enviroment. */
     envSetOwnProcessHandle(exl::util::proc_handle::Get());
@@ -74,18 +37,6 @@ extern "C" void exl_main(void* x0, void* x1) {
 
 #ifdef ROMFS_ENABLED
     // sv::feature::romfs::exl_main();
-#endif
-
-#ifdef CUSTOM_ENABLED
-    EXTERNAL_ADDRESS(lua_toboolean, 0xa44d90);
-    Hook_0x165d8c0::InstallAtOffset(0x165d8c0);
-    Hook_0x179bdd0::InstallAtOffset(0x179bdd0);
-    Hook_0x23e0780::InstallAtOffset(0x23e0780);
-    Hook_0x2411d30::InstallAtOffset(0x2411d30);
-    Hook_0x2421560::InstallAtOffset(0x2421560);
-    Hook_0x24264b0::InstallAtOffset(0x24264b0);
-
-    StringsFuncHook::InstallAtOffset(0x1312744);
 #endif
 
     runCodePatches();
